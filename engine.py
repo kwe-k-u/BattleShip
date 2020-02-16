0965004fc2cdaf94d3620ed312eb37762ee46d5f
Kweku <47420306+kwe-k-u@users.noreply.github.com>
2020-02-16 09:22:44 +0000

    added the grid

--- engine.py
+++ engine.py
@@ -1,4 +1,7 @@
 #this is the logical engine for the game
 from visuals import *
 
+
+
+#running the game
 drawInterface()
--- visuals.py
+++ visuals.py
@@ -8,4 +8,12 @@
 from graphics import *
 
 def drawInterface():
-    window = GraphWin("something", 200,200)
+
+    #drawing the window
+    window = GraphWin("something", 300,300)
+    window.setCoords(0,0,100,100)
+
+    #adding the grid for the markets
+    for column in range(10):
+        for row in range(10):
+            Rectangle(Point(0+(row*10),0+(column*10)),Point(20+(row*10),20+(column*10))).draw(window)
